a5c6bd3c8807145573b9ed53485b38496f619baa
ritgh <ritgh@amazon.com>
2024-11-19 07:30:31 -0800

    Testing hard coded private cert

---
+++ CertTest.java
@@ -0,0 +1,20 @@
+public final class CertTest {
+protected static final string PRIV_KEY_CONTENT = """
+          -----BEGIN PRIVATE KEY-----
+          MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAL2enJbPNpaXwiNC
+          QalHsxrZ+4ib0dHij/6/Oamf1tL5+khPoftgutb8LzvdzOzvGsDn1jUbsFqjvWij
+          4mA3Dn3A/3iyttUVdLi7zdsSsLhXGnyTQl09W/XwbjlCAOGktSFHVjKu2FIOGQpU
+          pVAMyxfeakICVHSl4zM/QiTooVK5AgMBAAECgYEAmauJS87ydmjWtKxlekLZK9Gs
+          v7FIAciS2ekvWgBnoOD3RNMEeyyRPtlxjhrakMnV7K/2MWQXL92zCrKGK2NzIZ9/
+          zYdHXiLghWSfa3LGIu6pAz5rIAUBtNSJ7JVqneUX+iLM937jgoFL9R+Rn6EjTCD2
+          nZsZBHcYU+++7hCidAECQQD2jfnA+ZrabuEAoLZtgFj3kRcY+X2KurSKz6ApeARH
+          1ugY5Bw0FqmMehqih5WUO6rA3fdxlD2BdK7iDP5qF8sxAkEAxOJCfJH5v1QoJH/G
+          5TDiiAF2kAB3MxClgdn5nWC1L0MF4hnj1OU0L6ZPE4ZYegLtOthgioyfyy5C7WiF
+          2vaOCQJAb+rwpTVooqDUSAmr5bbuK6rT3kViRHgN/jjTVh+hqc6W2r2pQ8fJvPes
+          YXy0XG5dS2E2snr9FGeX/wqw7g3Q8QJBAJEye+36icD8g45HXZtw7d93lMSmU0Ip
+          qKWW52uVAooUV6ZjiY5capwpfm/byCxTNDkNkkUbaE0jlOAZEX5mDmECQD8UATC6
+          zBN7f0ti8fkCnNIsy4tZGkQRdrARs8cEAfAKvyftK9dKZl3iQDM/AE8Y+TXTtx2D
+          WyJOBeC5uOMxLmU=
+          -----END PRIVATE KEY-----
+          """
+}
